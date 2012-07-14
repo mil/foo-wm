68efb95ccfe42148122c95f27a84d05f70931a63
Miles Sandlar <miles.sandlar@gmail.com>
2012-07-14 01:59:02 -0400

    Update header commands.h,cycleFocus/diretionFocus

--- src/commands.h
+++ src/commands.h
@@ -1,5 +1,6 @@
 char * nextToken(char ** tokenString);
 void handleCommand(char* request);
-void focus(int direction);
+void cycleFocus(int direction);
+void directionFocus(int direction);
 void containerize();
 void kill();
