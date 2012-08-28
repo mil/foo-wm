0b5e2ebb01cbf20cc2711f968b6b4e3db3daf81e
= <miles.sandlar@gmail.com>
2012-08-27 22:24:33 -0400

    Set client and container padding with IPC

--- src/commands.c
+++ src/commands.c
@@ -191,9 +191,13 @@ void mark(char * markName) {
 void set(char * property, char * value) {
 
   if (!strcmp(property, "client_padding")) {
-  } else if (!strcmp(property, "client_border_width"))
+
+  } else if (!strcmp(property, "client_border_width")) {
     border = atoi(value);
   } else if (!strcmp(property, "container_padding")) {
+    containerPadding = atoi(value);
+  } else if (!strcmp(property, "client_padding")) {
+    clientPadding = atoi(value);
   } else if (!strcmp(property, "screen_padding_top")) {
     screenPaddingTop = atoi(value);
   } else if (!strcmp(property, "screen_padding_left")) {
